265fb5a5c9bb3c9853bbed9781d24742fe6ffd17
cdthedrummer <113496461+cdthedrummer@users.noreply.github.com>
2024-12-29 10:55:00 -0500

    feat: add QuizApp class and provider setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/quiz_provider.dart';
 import 'screens/welcome_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const QuizApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class QuizApp extends StatelessWidget {
+  const QuizApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'QuizDumb',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        fontFamily: 'Quicksand',
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => QuizProvider()),
+      ],
+      child: MaterialApp(
+        title: 'QuizDumb',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          fontFamily: 'Quicksand',
+        ),
+        home: const WelcomeScreen(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: const WelcomeScreen(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }

b3e9fadf3cde47b720e24305d9b44951f80bf4a0
cdthedrummer <113496461+cdthedrummer@users.noreply.github.com>
2024-12-29 10:31:29 -0500

    fix: update QuizState with proper null safety

--- lib/models/quiz_state.dart
+++ lib/models/quiz_state.dart
@@ -1,72 +1,25 @@
-import 'package:flutter/foundation.dart';
-import 'question.dart';
-import 'result.dart';
+class QuizState {
+  final List<dynamic> answers;
+  final int currentIndex;
+  final bool isComplete;
 
-class QuizState extends ChangeNotifier {
-  final List<Question> questions;
-  final Map<int, List<String>> answers = {};
-  int currentQuestionIndex = 0;
+  QuizState({
+    List<dynamic>? answers,
+    this.currentIndex = 0,
+    this.isComplete = false,
+  }) : answers = answers ?? [];
 
-  QuizState({required this.questions}) {
-    debugPrint('Initializing QuizState with ${questions.length} questions');
-  }
+  int get length => answers.length;
 
-  Question get currentQuestion => questions[currentQuestionIndex];
-  bool get isLastQuestion => currentQuestionIndex == questions.length - 1;
-  double get progress => (currentQuestionIndex + 1) / questions.length;
-  List<String> get currentAnswers => answers[currentQuestionIndex] ?? [];
-
-  void selectOption(String option) {
-    debugPrint('Selected option: $option');
-    if (currentQuestion.isMultipleChoice) {
-      answers[currentQuestionIndex] = answers[currentQuestionIndex] ?? [];
-      final currentAnswers = answers[currentQuestionIndex]!;
-      if (currentAnswers.contains(option)) {
-        currentAnswers.remove(option);
-      } else {
-        currentAnswers.add(option);
-      }
-    } else {
-      answers[currentQuestionIndex] = [option];
-    }
-    debugPrint('Current answers: ${answers[currentQuestionIndex]}');
-    notifyListeners();
-  }
-
-  void nextQuestion() {
-    if (currentQuestionIndex < questions.length - 1) {
-      currentQuestionIndex++;
-      debugPrint('Moving to question ${currentQuestionIndex + 1}');
-      notifyListeners();
-    }
-  }
-
-  QuizResult calculateResults() {
-    Map<String, int> scores = {
-      'Strength': 3,
-      'Dexterity': 3,
-      'Constitution': 3,
-      'Intelligence': 3,
-      'Wisdom': 3,
-      'Charisma': 3,
-    };
-
-    debugPrint('Starting with base scores: $scores');
-
-    answers.forEach((questionIndex, selectedOptions) {
-      debugPrint('\nProcessing question ${questionIndex + 1}:');
-      final question = questions[questionIndex];
-      for (final option in selectedOptions) {
-        final attributes = question.attributes[option]!;
-        debugPrint('  Selected option "$option" adds: $attributes');
-        attributes.forEach((attribute, value) {
-          scores[attribute] = (scores[attribute] ?? 3) + value;
-          debugPrint('    $attribute now at: ${scores[attribute]}');
-        });
-      }
-    });
-
-    debugPrint('Final scores: $scores');
-    return QuizResult(scores: scores);
+  QuizState copyWith({
+    List<dynamic>? answers,
+    int? currentIndex,
+    bool? isComplete,
+  }) {
+    return QuizState(
+      answers: answers ?? this.answers,
+      currentIndex: currentIndex ?? this.currentIndex,
+      isComplete: isComplete ?? this.isComplete,
+    );
   }
 }

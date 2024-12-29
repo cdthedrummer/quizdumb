35eec4c53dbcf8492d67c5169b0686fc36197f2d
cdthedrummer <113496461+cdthedrummer@users.noreply.github.com>
2024-12-29 01:22:38 -0500

    Fix type errors in quiz provider

--- lib/providers/quiz_provider.dart
+++ lib/providers/quiz_provider.dart
@@ -2,61 +2,35 @@ import 'package:flutter/foundation.dart';
 import '../models/question.dart';
 
 class QuizProvider extends ChangeNotifier {
+  final List<Question> _questions;
+  Map<int, List<int>> _answers = {};
   int _currentIndex = 0;
-  final List<Question> _questions = [];
-  final Map<int, List<int>> _answers = {};
-  final Map<String, double> _scores = {
-    'strength': 0,
-    'dexterity': 0,
-    'constitution': 0,
-    'intelligence': 0,
-    'wisdom': 0,
-    'charisma': 0,
-  };
 
-  int get currentIndex => _currentIndex;
-  List<Question> get questions => List.unmodifiable(_questions);
-  Map<String, double> get scores => Map.unmodifiable(_scores);
-  double get progress => _questions.isEmpty ? 0 : (_currentIndex + 1) / _questions.length;
-  bool get isComplete => _currentIndex >= _questions.length;
-
-  void answerQuestion(List<int> selectedIndices) {
-    if (kDebugMode) {
-      print('Answering question $_currentIndex with $selectedIndices');
-    }
-    
-    _answers[_currentIndex] = selectedIndices;
-    _updateScores();
-    
-    if (_currentIndex < _questions.length - 1) {
-      _currentIndex++;
-      notifyListeners();
-    }
+  QuizProvider(this._questions) {
+    debugPrint('Initializing QuizProvider with ${_questions.length} questions');
   }
 
-  void _updateScores() {
-    // Reset scores
-    _scores.updateAll((key, value) => 0);
-    
-    // Calculate new scores
-    _answers.forEach((questionIndex, answerIndices) {
+  Question get currentQuestion => _questions[_currentIndex];
+  bool get isLastQuestion => _currentIndex == _questions.length - 1;
+  double get progress => (_currentIndex + 1) / _questions.length;
+  List<int> get currentAnswers => _answers[_currentIndex] ?? [];
+
+  void updateScores(Map<String, num> scores) {
+    debugPrint('Updating scores: $scores');
+    for (var questionIndex in _answers.keys) {
       final question = _questions[questionIndex];
-      answerIndices.forEach((answerIndex) {
-        question.attributeScores.forEach((attribute, scores) {
-          _scores[attribute] = (_scores[attribute] ?? 0) + scores[answerIndex];
-        });
-      });
-    });
-
-    if (kDebugMode) {
-      print('Updated scores: $_scores');
+      for (var answerIndex in _answers[questionIndex]!) {
+        if (answerIndex >= 0 && answerIndex < question.options.length) {
+          final option = question.options[answerIndex];
+          final attributes = question.attributeScores[option];
+          if (attributes != null) {
+            attributes.forEach((attribute, value) {
+              scores[attribute] = (scores[attribute] ?? 0) + value;
+            });
+          }
+        }
+      }
     }
-  }
-
-  void reset() {
-    _currentIndex = 0;
-    _answers.clear();
-    _updateScores();
     notifyListeners();
   }
 }

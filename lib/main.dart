7f134bf91526947b4e90d504b125d1bcf78a579d
cdthedrummer <113496461+cdthedrummer@users.noreply.github.com>
2025-01-01 14:51:11 -0500

    Implement basic Provider structure in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/quiz_provider.dart';
 import 'screens/welcome_screen.dart';
 
 void main() {
@@ -6,18 +8,20 @@ void main() {
 }
 
 class QuizApp extends StatelessWidget {
-  const QuizApp({Key? key}) : super(key: key);
+  const QuizApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'QuizDumb',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        fontFamily: 'Quicksand',
+    return ChangeNotifierProvider(
+      create: (_) => QuizProvider(),
+      child: MaterialApp(
+        title: 'Quiz App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const WelcomeScreen(),
       ),
-      home: const WelcomeScreen(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }

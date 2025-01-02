0b6c4a4d45a389b83898ff0f3d9e40df316e7fbe
cdthedrummer <113496461+cdthedrummer@users.noreply.github.com>
2025-01-02 00:19:09 -0500

    feat: Add "Select all that apply" hint and improve text visibility

--- lib/screens/quiz/components/multiple_choice_question.dart
+++ lib/screens/quiz/components/multiple_choice_question.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'quiz_option.dart';
 
 class MultipleChoiceQuestion extends StatelessWidget {
   final List<String> options;
@@ -15,34 +14,91 @@ class MultipleChoiceQuestion extends StatelessWidget {
     required this.onAnswerSelected,
   });
 
-  void _handleOptionTap(String option) {
-    if (isMultiSelect) {
-      final newSelection = List<String>.from(selectedAnswers);
-      if (selectedAnswers.contains(option)) {
-        newSelection.remove(option);
-      } else {
-        newSelection.add(option);
-      }
-      onAnswerSelected(newSelection);
-    } else {
-      onAnswerSelected([option]);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: options.length,
-      itemBuilder: (context, index) {
-        final option = options[index];
-        final isSelected = selectedAnswers.contains(option);
-        
-        return QuizOption(
-          text: option,
-          isSelected: isSelected,
-          onTap: () => _handleOptionTap(option),
-        );
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        if (isMultiSelect)
+          Padding(
+            padding: const EdgeInsets.only(bottom: 16.0),
+            child: Text(
+              'Select all that apply',
+              style: TextStyle(
+                color: Colors.white.withAlpha(200),
+                fontSize: 14,
+                fontStyle: FontStyle.italic,
+                fontFamily: 'Quicksand',
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: options.length,
+            itemBuilder: (context, index) {
+              final option = options[index];
+              final isSelected = selectedAnswers.contains(option);
+              
+              return _buildOption(context, option, isSelected);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildOption(BuildContext context, String option, bool isSelected) {
+    return GestureDetector(
+      onTap: () {
+        if (isMultiSelect) {
+          final newSelection = List<String>.from(selectedAnswers);
+          if (selectedAnswers.contains(option)) {
+            newSelection.remove(option);
+          } else {
+            newSelection.add(option);
+          }
+          onAnswerSelected(newSelection);
+        } else {
+          onAnswerSelected([option]);
+        }
       },
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 8.0),
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
+        decoration: BoxDecoration(
+          color: isSelected 
+              ? Colors.white.withAlpha(100)
+              : Colors.white.withAlpha(20),
+          borderRadius: BorderRadius.circular(12.0),
+          border: Border.all(
+            color: isSelected 
+                ? Colors.white
+                : Colors.white.withAlpha(50),
+            width: 2,
+          ),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Text(
+                option,
+                style: TextStyle(
+                  color: Colors.white.withAlpha(isSelected ? 255 : 200),
+                  fontSize: 16,
+                  fontFamily: 'Quicksand',
+                  fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+                ),
+              ),
+            ),
+            if (isSelected)
+              const Icon(
+                Icons.check_circle,
+                color: Colors.white,
+              ),
+          ],
+        ),
+      ),
     );
   }
 }

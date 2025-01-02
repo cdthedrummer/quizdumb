d877ea81da803cdc3f7d8b8cdbd246b656de05ba
cdthedrummer <113496461+cdthedrummer@users.noreply.github.com>
2025-01-02 00:02:46 -0500

    refactor: Update multiple choice component to use quiz option

--- lib/screens/quiz/components/multiple_choice_question.dart
+++ lib/screens/quiz/components/multiple_choice_question.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
+import 'quiz_option.dart';
 
 class MultipleChoiceQuestion extends StatelessWidget {
   final List<String> options;
-  final bool isMultiSelect;
   final List<String> selectedAnswers;
+  final bool isMultiSelect;
   final Function(List<String>) onAnswerSelected;
 
   const MultipleChoiceQuestion({
     super.key,
     required this.options,
-    required this.isMultiSelect,
     required this.selectedAnswers,
+    required this.isMultiSelect,
     required this.onAnswerSelected,
   });
+
+  void _handleOptionTap(String option) {
+    if (isMultiSelect) {
+      final newSelection = List<String>.from(selectedAnswers);
+      if (selectedAnswers.contains(option)) {
+        newSelection.remove(option);
+      } else {
+        newSelection.add(option);
+      }
+      onAnswerSelected(newSelection);
+    } else {
+      onAnswerSelected([option]);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,66 +36,13 @@ class MultipleChoiceQuestion extends StatelessWidget {
       itemBuilder: (context, index) {
         final option = options[index];
         final isSelected = selectedAnswers.contains(option);
-
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
-          child: Material(
-            color: Colors.transparent,
-            child: InkWell(
-              onTap: () => _handleOptionTap(option),
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  border: Border.all(
-                    color: isSelected ? Theme.of(context).primaryColor : Colors.grey,
-                    width: 2,
-                  ),
-                  color: isSelected ? Theme.of(context).primaryColor.withOpacity(0.1) : null,
-                ),
-                child: Row(
-                  children: [
-                    if (isMultiSelect)
-                      Checkbox(
-                        value: isSelected,
-                        onChanged: (_) => _handleOptionTap(option),
-                      )
-                    else
-                      Radio<bool>(
-                        value: true,
-                        groupValue: isSelected,
-                        onChanged: (_) => _handleOptionTap(option),
-                      ),
-                    const SizedBox(width: 12),
-                    Expanded(
-                      child: Text(
-                        option,
-                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                          color: isSelected ? Theme.of(context).primaryColor : null,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+        
+        return QuizOption(
+          text: option,
+          isSelected: isSelected,
+          onTap: () => _handleOptionTap(option),
         );
       },
     );
-  }
-
-  void _handleOptionTap(String option) {
-    if (isMultiSelect) {
-      final newAnswers = List<String>.from(selectedAnswers);
-      if (selectedAnswers.contains(option)) {
-        newAnswers.remove(option);
-      } else {
-        newAnswers.add(option);
-      }
-      onAnswerSelected(newAnswers);
-    } else {
-      onAnswerSelected([option]);
-    }
   }
 }

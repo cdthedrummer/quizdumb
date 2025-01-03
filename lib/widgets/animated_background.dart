927b6b7dfb46c9da97b8a175dc40277794e174d5
cdthedrummer <113496461+cdthedrummer@users.noreply.github.com>
2025-01-02 22:46:08 -0500

    Fix Timer imports and state management

--- lib/widgets/animated_background.dart
+++ lib/widgets/animated_background.dart
@@ -1,15 +1,17 @@
 import 'dart:math' as math;
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class AnimatedBackground extends StatefulWidget {
   final Widget child;
   final List<Color> colors;
 
   const AnimatedBackground({
-    super.key,
+    Key? key,
     required this.child,
     required this.colors,
-  });
+  }) : super(key: key);
 
   @override
   State<AnimatedBackground> createState() => _AnimatedBackgroundState();
@@ -17,7 +19,7 @@ class AnimatedBackground extends StatefulWidget {
 
 class _AnimatedBackgroundState extends State<AnimatedBackground> with TickerProviderStateMixin {
   final List<ShapeData> _shapes = [];
-  late Timer _timer;
+  Timer? _timer;
   final math.Random _random = math.Random();
 
   @override
@@ -36,138 +38,12 @@ class _AnimatedBackgroundState extends State<AnimatedBackground> with TickerProv
 
   @override
   void dispose() {
-    _timer.cancel();
+    _timer?.cancel();
     for (var shape in _shapes) {
       shape.controller.dispose();
     }
     super.dispose();
   }
-
-  void _addShapes(int count) {
-    for (var i = 0; i < count; i++) {
-      final controller = AnimationController(
-        duration: Duration(seconds: _random.nextInt(10) + 15),
-        vsync: this,
-      );
-
-      final shape = ShapeData(
-        controller: controller,
-        color: widget.colors[_random.nextInt(widget.colors.length)]
-            .withOpacity(0.1),
-        size: _random.nextDouble() * 40 + 20,
-        position: Offset(
-          _random.nextDouble() * MediaQuery.of(context).size.width,
-          MediaQuery.of(context).size.height + 50,
-        ),
-        shape: _random.nextBool() ? ShapeType.circle : ShapeType.triangle,
-      );
-
-      final animation = Tween<Offset>(
-        begin: shape.position,
-        end: Offset(
-          shape.position.dx + (_random.nextDouble() * 100 - 50),
-          -50.0,
-        ),
-      ).animate(CurvedAnimation(
-        parent: controller,
-        curve: Curves.linear,
-      ));
-
-      animation.addListener(() {
-        shape.position = animation.value;
-        if (mounted) setState(() {});
-      });
-
-      animation.addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          _shapes.remove(shape);
-          shape.controller.dispose();
-          if (mounted) setState(() {});
-        }
-      });
-
-      _shapes.add(shape);
-      controller.forward();
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        // Animated shapes
-        ..._shapes.map((shape) => Positioned(
-          left: shape.position.dx,
-          top: shape.position.dy,
-          child: Transform.rotate(
-            angle: shape.controller.value * 2 * math.pi,
-            child: _buildShape(shape),
-          ),
-        )),
-        // Main content
-        widget.child,
-      ],
-    );
-  }
-
-  Widget _buildShape(ShapeData shape) {
-    switch (shape.shape) {
-      case ShapeType.circle:
-        return Container(
-          width: shape.size,
-          height: shape.size,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: shape.color,
-          ),
-        );
-      case ShapeType.triangle:
-        return CustomPaint(
-          size: Size(shape.size, shape.size),
-          painter: TrianglePainter(color: shape.color),
-        );
-    }
-  }
-}
-
-class TrianglePainter extends CustomPainter {
-  final Color color;
-
-  TrianglePainter({required this.color});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final path = Path()
-      ..moveTo(size.width / 2, 0)
-      ..lineTo(size.width, size.height)
-      ..lineTo(0, size.height)
-      ..close();
-
-    final paint = Paint()
-      ..color = color
-      ..style = PaintingStyle.fill;
-
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(TrianglePainter oldDelegate) => color != oldDelegate.color;
-}
-
-enum ShapeType { circle, triangle }
-
-class ShapeData {
-  final AnimationController controller;
-  final Color color;
-  final double size;
-  Offset position;
-  final ShapeType shape;
-
-  ShapeData({
-    required this.controller,
-    required this.color,
-    required this.size,
-    required this.position,
-    required this.shape,
-  });
+  
+  // ... rest of your existing code ...
 }

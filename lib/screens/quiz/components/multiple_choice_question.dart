b97a69274b4eab4a2702a350c7234a7f9cc82fa7
cdthedrummer <113496461+cdthedrummer@users.noreply.github.com>
2025-01-03 11:26:10 -0500

    Update MultipleChoiceQuestion to use Material and InkWell for better touch handling

--- lib/screens/quiz/components/multiple_choice_question.dart
+++ lib/screens/quiz/components/multiple_choice_question.dart
@@ -49,54 +49,60 @@ class MultipleChoiceQuestion extends StatelessWidget {
   }
 
   Widget _buildOption(BuildContext context, String option, bool isSelected) {
-    return GestureDetector(
-      onTap: () {
-        if (isMultiSelect) {
-          final newSelection = List<String>.from(selectedAnswers);
-          if (selectedAnswers.contains(option)) {
-            newSelection.remove(option);
-          } else {
-            newSelection.add(option);
-          }
-          onAnswerSelected(newSelection);
-        } else {
-          onAnswerSelected([option]);
-        }
-      },
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 8.0),
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
-        decoration: BoxDecoration(
-          color: isSelected 
-              ? Colors.white.withAlpha(100)
-              : Colors.white.withAlpha(20),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () {
+            if (isMultiSelect) {
+              final newSelection = List<String>.from(selectedAnswers);
+              if (selectedAnswers.contains(option)) {
+                newSelection.remove(option);
+              } else {
+                newSelection.add(option);
+              }
+              onAnswerSelected(newSelection);
+            } else {
+              onAnswerSelected([option]);
+            }
+          },
           borderRadius: BorderRadius.circular(12.0),
-          border: Border.all(
-            color: isSelected 
-                ? Colors.white
-                : Colors.white.withAlpha(50),
-            width: 2,
-          ),
-        ),
-        child: Row(
-          children: [
-            Expanded(
-              child: Text(
-                option,
-                style: TextStyle(
-                  color: Colors.white.withAlpha(isSelected ? 255 : 200),
-                  fontSize: 16,
-                  fontFamily: 'Quicksand',
-                  fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
-                ),
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
+            decoration: BoxDecoration(
+              color: isSelected 
+                  ? Colors.white.withAlpha(100)
+                  : Colors.white.withAlpha(20),
+              borderRadius: BorderRadius.circular(12.0),
+              border: Border.all(
+                color: isSelected 
+                    ? Colors.white
+                    : Colors.white.withAlpha(50),
+                width: 2,
               ),
             ),
-            if (isSelected)
-              const Icon(
-                Icons.check_circle,
-                color: Colors.white,
-              ),
-          ],
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    option,
+                    style: TextStyle(
+                      color: Colors.white.withAlpha(isSelected ? 255 : 200),
+                      fontSize: 16,
+                      fontFamily: 'Quicksand',
+                      fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+                    ),
+                  ),
+                ),
+                if (isSelected)
+                  const Icon(
+                    Icons.check_circle,
+                    color: Colors.white,
+                  ),
+              ],
+            ),
+          ),
         ),
       ),
     );

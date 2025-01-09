cca5b2961c34c4549b7eba684e3a12b3d171b8ff
cdthedrummer <113496461+cdthedrummer@users.noreply.github.com>
2025-01-09 14:07:22 -0500

    Update Question model to support new categories and question types

--- lib/models/question.dart
+++ lib/models/question.dart
@@ -1,23 +1,48 @@
+import 'package:flutter/material.dart';
+import 'category.dart';
+
+enum QuestionType {
+  singleChoice,
+  multipleChoice,
+  scale,
+  imageChoice,
+  priorityRanking;
+
+  bool get isSingleChoice => this == QuestionType.singleChoice;
+  bool get isMultipleChoice => this == QuestionType.multipleChoice;
+  bool get isScale => this == QuestionType.scale;
+  bool get isImageChoice => this == QuestionType.imageChoice;
+  bool get isPriorityRanking => this == QuestionType.priorityRanking;
+}
+
 class Question {
-  final int id;
+  final String id;
   final String text;
-  final String type;
+  final QuestionType type;
+  final List<Category> categories;
+  final Map<String, double> categoryWeights;
   final List<String>? options;
-  final Map<String, Map<String, int>>? attributes;
-  final Map<String, int>? scaleAttributes;
+  final List<String>? imageOptions;
   final Map<int, String>? scaleLabels;
+  final String? encouragingMessage;
+  final bool isCheckpoint;
 
   const Question({
     required this.id,
     required this.text,
     required this.type,
+    required this.categories,
+    required this.categoryWeights,
     this.options,
-    this.attributes,
-    this.scaleAttributes,
+    this.imageOptions,
     this.scaleLabels,
+    this.encouragingMessage,
+    this.isCheckpoint = false,
   });
 
-  bool get isScale => type == 'scale';
-  bool get isSingleChoice => type == 'single';
-  bool get isMultipleChoice => type == 'multiple';
+  bool get requiresImages => type == QuestionType.imageChoice;
+  bool get requiresOptions => type == QuestionType.singleChoice || 
+                            type == QuestionType.multipleChoice || 
+                            type == QuestionType.priorityRanking;
+  bool get requiresScaleLabels => type == QuestionType.scale;
 }

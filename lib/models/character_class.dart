2c87b71373a16bc9a7542e9a0580b9d77b64ad18
cdthedrummer <113496461+cdthedrummer@users.noreply.github.com>
2025-01-09 14:25:51 -0500

    Add CharacterClass model

--- lib/models/character_class.dart
+++ lib/models/character_class.dart
@@ -1,30 +1,15 @@
 class CharacterClass {
   final String name;
-  final String title;
   final String emoji;
   final String description;
-  final String primaryStat;
-  final String secondaryStat;
-  final Map<String, int> minimumStats;
-  final List<String> keyTraits;
-  final List<String> growthAreas;
-  final List<String> suggestedActivities;
+  final Map<String, double> attributeWeights;
+  final List<String> recommendations;
 
   const CharacterClass({
     required this.name,
-    required this.title,
     required this.emoji,
     required this.description,
-    required this.primaryStat,
-    required this.secondaryStat,
-    required this.minimumStats,
-    required this.keyTraits,
-    required this.growthAreas,
-    required this.suggestedActivities,
+    required this.attributeWeights,
+    this.recommendations = const [],
   });
-
-  @override
-  String toString() {
-    return 'CharacterClass{name: $name, primary: $primaryStat, secondary: $secondaryStat}';
-  }
 }

8cd6b45cfff65041a7aa258614aa6f82c1c57d91
cdthedrummer <113496461+cdthedrummer@users.noreply.github.com>
2025-01-10 21:06:45 -0500

    fix: update welcome screen import path

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'providers/quiz_provider.dart';
-import 'screens/welcome/welcome_screen.dart';
+import 'screens/welcome_screen.dart';  // Fixed import
 import 'theme/app_theme.dart';
 
 void main() {
